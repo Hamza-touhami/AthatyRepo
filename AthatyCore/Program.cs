17ef8db9581793e14fe6b91ca3296d07c72eb283
mazzed <m.azzeddineennaoui@gmail.com>
2022-01-23 13:40:45 +0100

    Adding MongoDB client to AthatyCore

--- AthatyCore/Program.cs
+++ AthatyCore/Program.cs
@@ -1,3 +1,8 @@
+using AthatyCore.Settings;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Setting up MongoDB connection
+
+//Serializing GUID as string
+BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String));
+
+//Serializing DateTimeOffset as string
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(MongoDB.Bson.BsonType.String));
+
+var mongoDBSettings = builder.Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
+
+//Registering IMongoClient dependency (Dependency Injection)
+builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
+{
+    return new MongoClient(mongoDBSettings.ConnectionToken);
+});
 
 var app = builder.Build();
 
